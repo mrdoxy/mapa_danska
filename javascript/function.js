095400033644f4ed04676b14343e9ee41009d4bb
mrdoxy <47314973+mrdoxy@users.noreply.github.com>
2020-06-13 19:10:29 +0200

    Add files via upload

---
+++ javascript/function.js
@@ -0,0 +1,146 @@
+const mesta = [{
+        "nazev": "kodan",
+        "text": "Kodaň je hlavní a největší město Dánska. Žije zde přibližně 602 tisíc obyvatel. Kodaň je sídlem dánské královny. Původně to byla rybářská vesnice Vikingů v 10. století.",
+        "img": "img/Kodan.png"
+    },
+    {
+        "nazev": "Fyn",
+        "text": "Fyn je jedním z nejdůležitějších ostrovů Dánska. Zároveň je toto město třetí největší. Je jedním z nejstarších měst Dánska, v roce 1988 slavilo 1000 let založení. Rakev Svatého Knuta byla ve středověku oblíbeným poutním místem.",
+        "img": "img/Odense.png"
+    },
+    {
+        "nazev": "Alborg",
+        "text": "Aalborg je dánské město ležící na severu Jutského poloostrova. Žije zde přibližně 113 tisíc obyvatel a Aalborg je čtvrtým největším městem v Dánsku. Pro turisty je zde krásná zoologická zahrada a námořní muzeum s ponorkou z válečných dob.",
+        "img": "img/Aalborg.png"
+    },
+    {
+        "nazev": "Arhus",
+        "text": "Aarhus je druhé největší město Dánska, má 294 954 obyvatel. Město je významným námořním přístavem, hospodářským a kulturním střediskem Jutska a také sídlem druhé největší dánské univerzity (22 tisíc studentů). Historie Aarhusu začala jako opevněná osada Vikingů založená v 8. století.",
+        "img": "img/Aarhus.png"
+    },
+    {
+        "nazev": "Esbjerg",
+        "text": "Esbjerg je přístavní město na jihozápadě Dánska, je také 5. největší město.",
+        "img": "img/Esbjerg.png"
+    },
+    {
+        "nazev": "Viborg",
+        "text": "Viborg je dánské město ležící ve středním Jutsku. Viborg je také sídlem Nejvyššího soudu pro západní Dánsko. Město bylo založeno v 8. století Vikingy a je tak jedním z nejstarších měst.",
+        "img": "img/Viborg.png"
+    },
+    {
+        "nazev": "Hjorring",
+        "text": "Hjørring je jedno z nejstarších měst v Dánském království. v roce 1993 slavilo 750. svýročí. Městem vede každoroční moto-revue, která se zde pořádá již od roku 1920. Ve městě se nachází také mnoho prestižních dánských škol.",
+        "img": "img/Hjørring.png"
+    },
+    {
+        "nazev": "Holstebro",
+        "text": "Město vzniklo u brodu u potoka, později zde byl postaven most. Jméno pravděpodobně pochází z dánského holdested ved broen (místo odpočinku u mostu). V roce 1552 zde vypukl obrovský požár, který zničil mnoho starých budov.",
+        "img": "img/Holstebro.png"
+    },
+    {
+        "nazev": "Herning",
+        "text": "Herning je město v Dánsku v regionu Midtjylland na Jutském poloostrově. V roce 2018 se ve městě Herning uskutečnilo 82. Mistrovství světa v ledním hokeji. Bylo to vůbec poprvé v historii, kdy Dánsko uspořádalo hokejový světový šampionát.",
+        "img": "img/Herning.png"
+    }
+]
+const pamatky = [
+{
+    "id":"Christiansfeld",
+    "nazev": "Město Moravských bratří Christiansfeld",
+    "text": "Christiansfeld byl založen moravskými bratry v roce 1773, většina původní zástavby byla postavena do roku 1800. Město bylo pojmenováno na počest krále Kristiána VII, který městu odpustil na deset let daně a zaplatil deset procent výdajů na stavbu města. Christiansfeld je výjimečným urbanistickým konceptem, s výborně zachovalou původní architekturou, díky čemuž bylo město v roce 2015 město zapsáno na seznam světového kulturního dědictví UNESCO.",
+    "img": "img/Christiansfeld.jpg"
+},
+{
+    "id":"lod",
+    "nazev": "Fregata Jylland",
+    "text": "Jylland byla šroubová fregata dánského námořnictva třídy Niels Juel. Byla postavena v letech 1857–1862 loděnicí Nyholus Skibsverft. Dvouválcový parní stroj o výkonu 1300 HP. Loď bojovala roku 1864 v druhé válce o Šlesvicko-Holštýnsko. Jylland byl vlajkovou lodí kapitána Suensona v nerozhodném střetnutí s rakouskou eskadrou v bitvě u Helgolandu dne 9. května 1864.",
+    "img": "img/Jylland.jpg"
+},
+{
+    "id":"Kronborg",
+    "nazev": "Zámek Kronborg",
+    "text": "Kronborg je opevněný zámek, někdy je označován jako hrad, nacházející se v dánském městě Helsingør, na nejseverovýchodnějším cípu pobřeží ostrova Sjælland. Hrad byl založen pod jménem Krogen v roce 1420 králem Erikem VII Pomořanským. A sloužil díky své poloze také jako místo výběru cla od lodí plujících přes úžinu Öresund.",
+    "img": "img/Kronborg.jpg"
+},
+{
+    "id":"Runy",
+    "nazev": "Mohyly, runové kameny a kostel v Jellingu",
+    "text": "Mohyly, runové kameny a kostel v Jellingu jsou na Seznamu světového kulturního dědictví UNESCO zapsány od roku 1994 Runové kameny se nacházejí ve dvoře jellingského kostela mezi dvěma velkými mohylami. Celý tento soubor je svědectvím doby přechodu tehdejších skandinávců od pohanství ke křesťanství.",
+    "img": "img/mohyly.jpg"
+},
+{
+    "id":"Frederiksborg_",
+    "nazev": "Zámek Frederiksborg",
+    "text": "Frederiksborg je hrad v Hillerødu na dánském ostrově Sjælland. Nese jméno po dánském králi Frederiku II.1560 získal dánský král Frederik II. Bývalé panské sídlo Hillerødsholm a okolní pozemky a postavil zde nové sídlo, jež nazval svým jménem. Jeho syn Kristián IV. nechal původní stavbu odstranit a v letech 1602 - 1620 vybudoval podle projektů architektů Hanse a Lorenze van Steenwinckelových na třech malých ostrůvcích v Frederiksborgsee dnešní zámek - největší a nejvýznamnější stavbu severské renesance.",
+    "img": "img/Frederiksborg.jpg"
+},
+{
+    "id":"Katedr__laRoskilde_",
+    "nazev": "Katedrála v Roskilde",
+    "text": "Katedrála v Roskilde je jednou z dánských památek, které najdeme na Seznamu světového dědictví UNESCO. Na tento seznam je katedrála zapsaná od roku 1995. Vznik současné katedrály v Roskilde (Roskilde Domkirke) se datuje do poloviny dvanáctého století. Budova je cihlovou trojlodní stavbou, která je obklopena věncem kaplí. V těchto kaplí se nachází část královských hrobů, jiné jsou například v presbytáři nebo v kryptě kostela. V této katedrále je pohřbeno více než čtyřicet dánských králů a královen.",
+    "img": "img/katedrala.jpg"
+},
+{
+    "id":"piskove_sochy",
+    "nazev": "Sandskulpturfestival i Soendervig",
+    "text": "Kdo by to byl řekl, na větry ošlehaném severském kontinentu se poblíž města Soendervig pořádá každoroční festival piskovcových soch.",
+    "img": "img/socha.jpg"
+}
+]
+
+/* jQuery skript */
+$(function () {
+    let color = null;
+
+    //státy
+    $('path').mouseover(function () {
+        color = $(this).css('fill');
+        $(this).css('fill', 'red');
+    })
+    $('path').on('mouseleave', function () {
+        $(this).css('fill', color);
+    })
+    $('[data-toggle="popover"]').popover();
+
+    //města
+    $('rect').mouseover(function () {
+        color = $(this).css('fill');
+        $(this).css('fill', 'white');
+    })
+    $('rect').on('mouseleave', function () {
+        $(this).css('fill', color);
+    })
+    $('rect').on('click', function () {
+        let mesto = mesta.find(item => {
+            return item.nazev == $(this).attr('id')
+        });
+
+        $('h2').text(mesto.nazev);
+        $('p').text(mesto.text);
+        $('img').attr('src', mesto.img);
+        $('img').attr('style',"width: 300px");
+    })
+
+    //památky
+    $('ellipse').mouseover(function () {
+        color = $(this).css('fill');
+        $(this).css('fill', 'black');
+    })
+    $('ellipse').on('mouseleave', function () {
+        $(this).css('fill', color);
+    })
+    $('ellipse').on('click', function () {
+        $('h2').text($(this).find('title').text())
+        $(this).css('fill', 'black');
+
+        let pamatka = pamatky.find(item => {
+            return item.id == $(this).attr('id')
+        });
+
+        $('h2').text(pamatka.nazev);
+        $('p').text(pamatka.text);
+        $('img').attr('src', pamatka.img);
+        $('img').attr('style',"width: 100%;")
+    })
+})
